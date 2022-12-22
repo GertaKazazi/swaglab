3e439fbb14c326a7a7b367b71c6ec80535744a5e
GertaK <gertakazazi@gmail.com>
2022-12-22 11:19:40 +0100

    testing git 2

---
+++ src/main/java/org/SwagLab/TestCases/AddToCart.java
@@ -0,0 +1,47 @@
+package org.SwagLab.TestCases;
+
+import org.SwagLab.Pages.LoginPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AddToCart {
+    @Test
+
+    public void loginTest() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        //Open test page
+        String url = "https://www.saucedemo.com/";
+        driver.get(url);
+        System.out.println("Page is opened!");
+        //Maximize browser window
+        driver.manage().window().maximize();
+        Thread.sleep(1000);
+
+
+        LoginPage login = new LoginPage(driver);
+        login.typeUserName();
+        Thread.sleep(500);
+        login.typePassword();
+        Thread.sleep(500);
+        login.clickOnLoginButton();
+        Thread.sleep(500);
+
+        //Scroll to end
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,-350)", "");
+        Thread.sleep(2000);
+        // Add to cart
+        driver.findElement(By.id("add-to-cart-sauce-labs-onesie")).click();
+        Thread.sleep(2000);
+
+
+
+
+    }
+}
