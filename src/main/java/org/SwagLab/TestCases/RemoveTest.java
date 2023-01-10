9e1a30008a12fe40c0a12d4682f02279e8ccd7d2
GertaK <gertakazazi@gmail.com>
2023-01-10 15:53:42 +0100

    Update Base Test & Remove Test case

--- src/main/java/org/SwagLab/TestCases/RemoveTest.java
+++ src/main/java/org/SwagLab/TestCases/RemoveTest.java
@@ -1,2 +1,24 @@
-package org.SwagLab.TestCases;public class RemoveTest {
+package org.SwagLab.TestCases;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import java.util.List;
+
+public class RemoveTest extends AddToCartTest {
+    @Test
+    public void removeTest() {
+        //Remove the lowest item product
+        List<WebElement> products = driver.findElements(By.className("cart_item"));
+        double min = Double.MAX_VALUE;
+        for (WebElement product : products) {
+            String elementText = product.findElement(By.className("inventory_item_price")).getText();
+            String remove = product.findElement(By.className("cart_item_label")).getText();
+            elementText = elementText.substring(1);
+            double ele = Double.parseDouble(elementText);
+            if (ele < min & remove.contains("REMOVE")) {
+                min = ele;
+                driver.findElement(By.xpath("//button[@class='btn btn_secondary btn_small cart_button']")).click();
+            }
+        }
+    }
 }
